40dbdfe194ce291dcc2413677d8ab2f4bab0566f
EricSnyder622 <esnyder276@g.rwu.edu>
2021-10-23 18:13:59 -0400

    Assignment5 commit - 3 branches

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import sys
+import datetime
 
-def sysExample():
-
+def datetimeExample():
+    now = datetime.datetime.now()
+    print(now.year, "\n", now.strftime("%A"))
+    now = datetime.datetime(2010, 6, 22)
+    print(now.year, "\n", now.strftime("%A"))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    sysExample()
+    datetimeExample()
