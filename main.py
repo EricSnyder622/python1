c659c5f6495776e4eef76a75c68726901ee7a11e
EricSnyder622 <esnyder276@g.rwu.edu>
2021-10-23 17:59:35 -0400

    Assignment5 commit - 3 branches

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
 def osExample():
-    print("OS Name: " + os.name)
+    print("OS Name: " + os.name + "\n Working Directory: " + os.getcwd())
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     osExample()
