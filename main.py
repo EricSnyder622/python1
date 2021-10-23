24ebb4b188440f19909abe94735e5ff64ad0a4f0
EricSnyder622 <esnyder276@g.rwu.edu>
2021-10-23 17:42:52 -0400

    Assignment5 commit - master

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 # This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 def assignment2():
     # get users name
     name = input("Tell me your name:")
     print(f'Hello {name}')
-
     # enter a number and multiply by itself
     x = int(input("Enter a number: "))
     print(x * x)
-
     #enter a word and calculate letter number
     word = input("Please enter a word: ")
     print("The number of letters in the word is: ", len(word))
@@ -28,7 +22,6 @@ def assignment3():
     grid = [[0, 0, 0, 0, 0]]
     for i in range(rows):
         print("".join('.' for j in range(col)))
-
     #Coin Flip
     coin = int(input("Enter a value 1 or 0: "))
     flip = random.randint(0, 1)
@@ -106,6 +99,11 @@ def victory_for(board, sign):
         return who
     return None
 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # print_hi('PyCharm')
+    #assignment2()
+    #assignment3()
 
 def draw_move(board):
     # make a list of free fields
@@ -146,3 +144,4 @@ else:
 
 # Press the green button in the gutter to run the script.
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
