4055f31e870343cf92fbd216d97a2a8d05238d37
EricSnyder622 <esnyder276@g.rwu.edu>
2021-10-23 18:07:09 -0400

    Assignment5 commit - 3 branches

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 
 def sysExample():
-
+    print("System path: ", sys.path, "\nMaxsize: ", sys.maxsize)
+    print("\nVersion: ", sys.version)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
