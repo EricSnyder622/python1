00e9d71bd25707a015be10bc2fca9f02f9a11a9b
EricSnyder622 <esnyder276@g.rwu.edu>
2021-10-30 23:19:56 -0400

    Assignment6 strings2

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ def user_input():
     return user_string
 
 def mod_text():
-
+    orig_string = user_input()
+    temp_string = original_string.split()
+    for index, letter in enumerate(orig_string, 1):
+        print(index, ':', letter)
+    print(temp_string)
+    last_char = orig_string[-1]
+    print(last_char)
+    without_last = orig_string[:-1]
+    print(without_last)
 
 if __name__ == '__main__':
   mod_text()
